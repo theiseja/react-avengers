d18354425126c5725f5a1f7e80c62af31f3f9c87
Jesse <theiseja@gmail.com>
2018-10-09 22:00:25 -0500

    Made it so the avengers array is displayed on the page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      avengers: ['Captain America', 'Black Widow', 'Thor', 'Hulk', 'Iron-Man', 'Hawkeye']
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -11,6 +17,7 @@ class App extends Component {
           <p>
            Avengers: by Jesse Theisen
           </p>
+          {this.state.avengers.map(avenger => <div key={avenger}>{avenger}</div>)}
           <a
             className="App-link"
             href="https://reactjs.org"
